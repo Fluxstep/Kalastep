98f1d6b5c6e660966ace0b384cb3752cfe10b8fe
Fluxstep <Ougaammoui@gmail.com>
2026-06-23 22:52:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -915,7 +915,7 @@ async def daily(ctx):
             color=discord.Color.orange()
         )
         embed.set_footer(text="Made by Fluxstep")
-       try:
+            try:
     await ctx.author.send(embed=embed)
 except:
     await ctx.reply("❌ I couldn't DM you. Please enable DMs.", delete_after=5)
