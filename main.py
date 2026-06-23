9f65373ef3dec1900f637bb902e4bc9e84aaf5e6
Fluxstep <Ougaammoui@gmail.com>
2026-06-23 18:53:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ from datetime import datetime, timedelta
 import time
 import random
 import asyncio
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 
 # ================= CONFIG =================
 
