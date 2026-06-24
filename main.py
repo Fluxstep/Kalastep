35ba65606069cb4e837bf76a92cec39a03183be2
Fluxstep <Ougaammoui@gmail.com>
2026-06-24 16:59:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ import time
 import random
 import asyncio
 import os
-import psycopg2
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-conn = psycopg2.connect(DATABASE_URL)
-cur = conn.cursor()
 
 # ================= CONFIG =================
 
